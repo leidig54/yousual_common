72204f5da9ef3752cd06f6f708f06dd6d4a95697
George Leidig <56688570+leidig54@users.noreply.github.com>
2022-09-24 07:12:56 -0700

    stuff

--- lib/models/extra.dart
+++ lib/models/extra.dart
@@ -80,7 +80,7 @@ class Extra {
               map['nutritionalInfo'] as Map<String, dynamic>)
           : null,
       ingredients: List<Ingredient>.from(
-        (map['ingredients'] as List<int>).map<Ingredient>(
+        (map['ingredients'] as List<dynamic>).map<Ingredient>(
           (x) => Ingredient.fromMap(x as Map<String, dynamic>),
         ),
       ),
