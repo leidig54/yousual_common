cff1748c14a06a863eb91c983bafbe5b723aaa78
George Leidig <56688570+leidig54@users.noreply.github.com>
2022-09-29 23:19:22 +0000

    feat(models/variable): Add price support to options in Variable model
    - Updated price calculation in Item model to include Option prices from Variable options.
    - Changed options list in Variable model to hold Option type instead of String.

--- lib/models/variable.dart
+++ lib/models/variable.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:yousual_common/models/ingredient.dart';
 import 'package:yousual_common/models/nutritional_info.dart';
+import 'package:yousual_common/models/option.dart';
 import 'package:yousual_common/models/tag.dart';
 
 class Variable {
@@ -9,14 +10,13 @@ class Variable {
   String variableName;
   String vendorId;
   String variableDescription;
-  double price;
   DateTime createdDateTime;
   DateTime? updatedDateTime;
   NutritionalInfo? nutritionalInfo;
   List<Ingredient> ingredients;
   List<Tag> tags;
   bool isRequired;
-  List<String> options;
+  List<Option> options;
   // enum
   TypeOfVariable variableType;
 
@@ -26,7 +26,6 @@ class Variable {
     required this.variableName,
     required this.vendorId,
     required this.variableDescription,
-    required this.price,
     required this.createdDateTime,
     this.updatedDateTime,
     this.nutritionalInfo,
@@ -38,28 +37,26 @@ class Variable {
   });
 
   Variable copyWith({
-    String? extraId,
+    String? variableId,
     String? itemId,
-    String? extraName,
+    String? variableName,
     String? vendorId,
     String? variableDescription,
-    double? price,
     DateTime? createdDateTime,
     DateTime? updatedDateTime,
     NutritionalInfo? nutritionalInfo,
     List<Ingredient>? ingredients,
     List<Tag>? tags,
     bool? isRequired,
-    List<String>? options,
+    List<Option>? options,
     TypeOfVariable? variableType,
   }) {
     return Variable(
-      variableId: extraId ?? variableId,
+      variableId: variableId ?? this.variableId,
       itemId: itemId ?? this.itemId,
-      variableName: extraName ?? variableName,
+      variableName: variableName ?? this.variableName,
       vendorId: vendorId ?? this.vendorId,
       variableDescription: variableDescription ?? this.variableDescription,
-      price: price ?? this.price,
       createdDateTime: createdDateTime ?? this.createdDateTime,
       updatedDateTime: updatedDateTime ?? this.updatedDateTime,
       nutritionalInfo: nutritionalInfo ?? this.nutritionalInfo,
@@ -73,31 +70,29 @@ class Variable {
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'extraId': variableId,
+      'variableId': variableId,
       'itemId': itemId,
-      'extraName': variableName,
+      'variableName': variableName,
       'vendorId': vendorId,
       'variableDescription': variableDescription,
-      'price': price,
       'createdDateTime': createdDateTime.millisecondsSinceEpoch,
       'updatedDateTime': updatedDateTime?.millisecondsSinceEpoch,
       'nutritionalInfo': nutritionalInfo?.toMap(),
       'ingredients': ingredients.map((x) => x.toMap()).toList(),
       'tags': tags.map((x) => x.toMap()).toList(),
       'isRequired': isRequired,
-      'options': options,
+      'options': options.map((x) => x.toMap()).toList(),
       'variableType': variableType.index,
     };
   }
 
   factory Variable.fromMap(Map<String, dynamic> map) {
     return Variable(
-      variableId: (map['extraId'] ?? '') as String,
+      variableId: (map['variableId'] ?? '') as String,
       itemId: (map['itemId'] ?? '') as String,
-      variableName: (map['extraName'] ?? '') as String,
+      variableName: (map['variableName'] ?? '') as String,
       vendorId: (map['vendorId'] ?? '') as String,
       variableDescription: (map['variableDescription'] ?? '') as String,
-      price: (map['price'] ?? 0.0) as double,
       createdDateTime: DateTime.fromMillisecondsSinceEpoch(
           (map['createdDateTime'] ?? 0) as int),
       updatedDateTime: map['updatedDateTime'] != null
@@ -119,8 +114,11 @@ class Variable {
         ),
       ),
       isRequired: (map['isRequired'] ?? false) as bool,
-      options: List<String>.from(
-          (map['options'] ?? const <String>[]) as List<String>),
+      options: List<Option>.from(
+        (map['options'] ?? const []).map(
+          (x) => Option.fromMap(x as Map<String, dynamic>),
+        ),
+      ),
       variableType: TypeOfVariable.values[(map['variableType'] ?? 0) as int],
     );
   }
