e3ac63fc602fe9ef8aeb5e56e35337ee2fea9c70
George Leidig <56688570+leidig54@users.noreply.github.com>
2022-10-06 21:50:04 +0100

    stuff

--- lib/models/vendor.dart
+++ lib/models/vendor.dart
@@ -1,45 +1,47 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
 class Vendor {
   String vendorId;
   String vendorName;
-  List<String> categories;
   DateTime createdDateTime;
+  bool isTakingOrders;
   Vendor({
     required this.vendorId,
     required this.vendorName,
-    this.categories = const [],
     required this.createdDateTime,
+    required this.isTakingOrders,
   });
 
   Vendor copyWith({
     String? vendorId,
     String? vendorName,
-    List<String>? categories,
     DateTime? createdDateTime,
+    bool? isTakingOrders,
   }) {
     return Vendor(
       vendorId: vendorId ?? this.vendorId,
       vendorName: vendorName ?? this.vendorName,
-      categories: categories ?? this.categories,
       createdDateTime: createdDateTime ?? this.createdDateTime,
+      isTakingOrders: isTakingOrders ?? this.isTakingOrders,
     );
   }
 
   Map<String, dynamic> toMap() {
-    return {
+    return <String, dynamic>{
       'vendorId': vendorId,
       'vendorName': vendorName,
-      'categories': categories,
       'createdDateTime': createdDateTime.millisecondsSinceEpoch,
+      'isTakingOrders': isTakingOrders,
     };
   }
 
   factory Vendor.fromMap(Map<String, dynamic> map) {
     return Vendor(
-      vendorId: map['vendorId'] ?? '',
-      vendorName: map['vendorName'] ?? '',
-      categories: List<String>.from(map['categories'] ?? const []),
-      createdDateTime:
-          DateTime.fromMillisecondsSinceEpoch(map['createdDateTime']),
+      vendorId: (map['vendorId'] ?? '') as String,
+      vendorName: (map['vendorName'] ?? '') as String,
+      createdDateTime: DateTime.fromMillisecondsSinceEpoch(
+          (map['createdDateTime'] ?? 0) as int),
+      isTakingOrders: (map['isTakingOrders'] ?? false) as bool,
     );
   }
 }
