0428db3fab3f6c175273e4337f251b00c48f3de4
George Leidig <56688570+leidig54@users.noreply.github.com>
2022-10-06 15:57:39 +0000

    feat(models/ingredient): Add ingredientId field to Ingredient model
    - Updated version number to 0.0.36 in pubspec.yaml

--- lib/models/ingredient.dart
+++ lib/models/ingredient.dart
@@ -1,21 +1,25 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Ingredient {
   String name;
+  String ingredientId;
   String vendorId;
   List<String> itemIds;
   Ingredient({
     required this.name,
+    required this.ingredientId,
     required this.vendorId,
     required this.itemIds,
   });
 
   Ingredient copyWith({
     String? name,
+    String? ingredientId,
     String? vendorId,
     List<String>? itemIds,
   }) {
     return Ingredient(
       name: name ?? this.name,
+      ingredientId: ingredientId ?? this.ingredientId,
       vendorId: vendorId ?? this.vendorId,
       itemIds: itemIds ?? this.itemIds,
     );
@@ -24,6 +28,7 @@ class Ingredient {
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'name': name,
+      'ingredientId': ingredientId,
       'vendorId': vendorId,
       'itemIds': itemIds,
     };
@@ -32,6 +37,7 @@ class Ingredient {
   factory Ingredient.fromMap(Map<String, dynamic> map) {
     return Ingredient(
       name: (map['name'] ?? '') as String,
+      ingredientId: (map['ingredientId'] ?? '') as String,
       vendorId: (map['vendorId'] ?? '') as String,
       itemIds: List<String>.from((map['itemIds'] ?? const <String>[])),
     );
