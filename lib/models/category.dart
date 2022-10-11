051994c6c27cdd15cee381c3c8823303c74a5aec
George Leidig <56688570+leidig54@users.noreply.github.com>
2022-10-11 12:36:55 +0000

    feat(category): Add itemIds to ItumCategory model
    - Updated the ItumCategory model to include itemIds as a new attribute.
    - Modified the copyWith method to cover the new itemIds field.
    - Updated fromMap factory method to handle itemIds.

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,23 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class ItumCategory {
   String categoryName;
   String categoryId;
   String vendorId;
   String description;
   int index;
+  List<String> itemIds = [];
   ItumCategory({
     required this.categoryName,
     required this.categoryId,
     required this.vendorId,
     required this.description,
     required this.index,
+    required this.itemIds,
   });
-
+//
   ItumCategory copyWith({
     String? categoryName,
     String? categoryId,
     String? vendorId,
     String? description,
     int? index,
+    List<String>? itemIds,
   }) {
     return ItumCategory(
       categoryName: categoryName ?? this.categoryName,
@@ -25,26 +29,29 @@ class ItumCategory {
       vendorId: vendorId ?? this.vendorId,
       description: description ?? this.description,
       index: index ?? this.index,
+      itemIds: itemIds ?? this.itemIds,
     );
   }
 
   Map<String, dynamic> toMap() {
-    return {
+    return <String, dynamic>{
       'categoryName': categoryName,
       'categoryId': categoryId,
       'vendorId': vendorId,
       'description': description,
       'index': index,
+      'itemIds': itemIds,
     };
   }
 
   factory ItumCategory.fromMap(Map<String, dynamic> map) {
     return ItumCategory(
-      categoryName: map['categoryName'] ?? '',
-      categoryId: map['categoryId'] ?? '',
-      vendorId: map['vendorId'] ?? '',
-      description: map['description'] ?? '',
-      index: map['index']?.toInt() ?? 0,
+      categoryName: (map['categoryName'] ?? '') as String,
+      categoryId: (map['categoryId'] ?? '') as String,
+      vendorId: (map['vendorId'] ?? '') as String,
+      description: (map['description'] ?? '') as String,
+      index: (map['index'] ?? 0) as int,
+      itemIds: List<String>.from((map['itemIds'] ?? const <String>[])),
     );
   }
 }
