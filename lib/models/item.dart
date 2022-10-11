b02bc44e1c1b4402af3f8211046eac752ad1b655
George Leidig <56688570+leidig54@users.noreply.github.com>
2022-10-11 13:38:57 +0100

    remove category from item

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 import 'package:intl/intl.dart';
-import 'package:yousual_common/models/category.dart';
 import 'package:yousual_common/models/ingredient.dart';
 import 'package:yousual_common/models/nutritional_info.dart';
 import 'package:yousual_common/models/tag.dart';
@@ -18,7 +17,6 @@ class Item {
   double basePrice;
   bool isAvailable;
   int index;
-  ItumCategory? itumCategory;
   DateTime createdDateTime;
   DateTime? updatedDateTime;
   NutritionalInfo? nutritionalInfo;
@@ -47,7 +45,6 @@ class Item {
     required this.basePrice,
     this.isAvailable = true,
     required this.index,
-    this.itumCategory,
     required this.createdDateTime,
     this.updatedDateTime,
     this.nutritionalInfo,
@@ -65,7 +62,6 @@ class Item {
     double? basePrice,
     bool? isAvailable,
     int? index,
-    ItumCategory? itumCategory,
     DateTime? createdDateTime,
     DateTime? updatedDateTime,
     NutritionalInfo? nutritionalInfo,
@@ -82,7 +78,6 @@ class Item {
       basePrice: basePrice ?? this.basePrice,
       isAvailable: isAvailable ?? this.isAvailable,
       index: index ?? this.index,
-      itumCategory: itumCategory ?? this.itumCategory,
       createdDateTime: createdDateTime ?? this.createdDateTime,
       updatedDateTime: updatedDateTime ?? this.updatedDateTime,
       nutritionalInfo: nutritionalInfo ?? this.nutritionalInfo,
@@ -102,7 +97,6 @@ class Item {
       'basePrice': basePrice,
       'isAvailable': isAvailable,
       'index': index,
-      'itumCategory': itumCategory?.toMap(),
       'createdDateTime': createdDateTime.millisecondsSinceEpoch,
       'updatedDateTime': updatedDateTime?.millisecondsSinceEpoch,
       'nutritionalInfo': nutritionalInfo?.toMap(),
@@ -124,9 +118,6 @@ class Item {
       basePrice: (map['basePrice'] ?? 0.0) as double,
       isAvailable: (map['isAvailable'] ?? false) as bool,
       index: (map['index'] ?? 0) as int,
-      itumCategory: map['itumCategory'] != null
-          ? ItumCategory.fromMap(map['itumCategory'] as Map<String, dynamic>)
-          : null,
       createdDateTime: DateTime.fromMillisecondsSinceEpoch(
           (map['createdDateTime'] ?? 0) as int),
       updatedDateTime: map['updatedDateTime'] != null
